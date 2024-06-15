adf157e13b225d390ef06bf271adf9dbaa2ebdb1
Arnav Bansal <70932975+arnavbansal2764@users.noreply.github.com>
2024-06-15 12:33:57 +0530

    Products

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { AlertModal } from "@/components/modals/alert-modal";
+import { ApiAlert } from "@/components/ui/api-alert";
 import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Heading } from "@/components/ui/heading";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
+import { useOrigin } from "@/hooks/use-origin";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Store } from "@prisma/client";
 import axios from "axios";
@@ -27,6 +29,7 @@ type SettingsFormValues = z.infer<typeof formSchema>;
 const SettingsForm: React.FC<SettingsFormProps> = ({
     initialData
 }) => {
+    const origin = useOrigin();
     const [open, setOpen] = useState(false);
     const [loading, setLoading] = useState(false);
 
@@ -37,38 +40,38 @@ const SettingsForm: React.FC<SettingsFormProps> = ({
     const params = useParams();
     const router = useRouter();
     const onSubmit = async (data: SettingsFormValues) => {
-        try{
+        try {
             setLoading(true);
-            await axios.patch(`/api/stores/${params.storeId}`,data);
+            await axios.patch(`/api/stores/${params.storeId}`, data);
             router.refresh();
             toast.success("Store Updated.");
-        }catch(error){
+        } catch (error) {
             toast.error("Something Went Wrong.");
         }
-        finally{
+        finally {
             setLoading(false);
         }
     };
-    const onDelete = async()=>{
-        try{
+    const onDelete = async () => {
+        try {
             setLoading(true);
             await axios.delete(`/api/stores/${params.storeId}`)
             router.refresh();
             router.push("/")
             toast.success("Store Deleted")
-        }catch(error){
+        } catch (error) {
             toast.error("Make sure you removed all products and categories first.");
-        }finally{
+        } finally {
             setLoading(false)
             setOpen(false)
         }
     }
     return (
         <>
-            <AlertModal isOpen={open} onClose={()=>setOpen(false)} onConfirm={onDelete} loading = {loading}/>
+            <AlertModal isOpen={open} onClose={() => setOpen(false)} onConfirm={onDelete} loading={loading} />
             <div className="flex items-center justify-between">
                 <Heading title="Settings" description="Manage store preferences" />
-                <Button disabled={loading} variant="destructive" size="icon" onClick={()=>setOpen(true)}>
+                <Button disabled={loading} variant="destructive" size="icon" onClick={() => setOpen(true)}>
                     <Trash className="h-4 w-4" />
 
                 </Button>
@@ -98,6 +101,8 @@ const SettingsForm: React.FC<SettingsFormProps> = ({
                     </Button>
                 </form>
             </Form>
+            <Separator />
+            <ApiAlert title="NEXT_PUBLIC_API_URL" description={`${origin}/api/${params.storeId}`} variant="public" />
         </>
 
     );
